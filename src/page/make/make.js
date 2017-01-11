390a66c545d7edcfbe5e3108385a368cba61f511
make <make@makedeMacBook-Pro.local>
2017-01-11 18:04:00 +0800

    make

--- src/page/make/make.js
+++ src/page/make/make.js
@@ -10,3 +10,20 @@ var content=$('#content')
 
 content.append(navHtml)
 content.append(pageHtml)
+
+const tabs=$('#rightTab a');
+tabs.on('click',function(e){
+  e.preventDefault()
+  $(this).tab('show')
+  const index=$(this).attr('target-index');
+  return $('.page-config').hide().eq(index).show()
+})
+
+$(()=>{
+
+  tabs.eq(0).trigger('click')
+
+  // 设置
+  
+
+})
